509a616e1c927d1a0767d191cb05ffb6747a4573
stankamchev <stanimir.kamchev@gmail.com>
2021-07-14 10:22:06 +0300

    added select option and some styles

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -6,9 +6,17 @@ const Main = () => {
             <form>
                 <input 
                 type="text" 
-                className="w-full sm:w-3/5 h-12 outline-none shadow-sm px-12"
+                className="w-full sm:w-3/5 h-12 outline-none shadow-sm px-12 mb-12"
                 placeholder="Search for a country..."/>
             </form>
+            <select className="h-12 w-1/2 bg-white px-4 mb-6">
+                <option value="">Filter By Region</option>
+                <option value="africa">Africa</option>
+                <option value="America">America</option>
+                <option value="asia">Asia</option>
+                <option value="europe">Europe</option>
+                <option value="oceania">Oceania</option>
+            </select>
         </div>
     )
 }
--- src/components/SingleCountry/index.jsx
+++ src/components/SingleCountry/index.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 
 const SingleCountry = ({key,flag,name,population,region,capital}) => {
     return (
-        <article className="flex justify-center">
-        <div className="flex-col w-4/6 max-w-screen-sm bg-white rounded-md mb-10 shadow-md">
+        <div className="hi">
+        <article className="flex justify-center ">
+        <div className="flex-col w-4/6 max-w-sm bg-white rounded-md mb-10 shadow-md">
             
-            <div key={key}>
+            <div className="grid grid-col-2" key={key}>
                 <img src={flag} alt={name} />
                 <div className="px-5 pb-5">
                 <h1 className="my-3 font-bold text-2xl">{name}</h1>
@@ -26,6 +27,7 @@ const SingleCountry = ({key,flag,name,population,region,capital}) => {
               
         </div>
         </article>
+        </div>
     )
 }
 
